d28d1e5c8794175da65965a35f3036790a9b5af4
omerbensaraf <39055406+omerbensaraf@users.noreply.github.com>
2018-05-22 14:17:59 +0300

    Elbitube wwebsite template

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: [
+    './footer.component.scss',
+    '../css/bootstrap.mincbed.css',
+    '../css/font-awesome.mincbed.css',
+    '../css/googleCss.css',
+    '../css/megamenucbed.css',
+    '../css/owl.carouselcbed.css',
+    '../css/pgwslider.mincbed.css',
+    '../css/style.mincbed.css'
+  ]
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
