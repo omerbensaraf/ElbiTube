8510a6b64f6eddf7fd656d67a555d8191b1dcffa
TC02363 <alon.yeshurun@elbitsystems.com>
2018-06-25 08:28:15 +0300

    changed server ip in media get method

--- src/app/services/media.service.ts
+++ src/app/services/media.service.ts
@@ -48,7 +48,7 @@ constructor(private http : HttpClient) {
 
 //httpGetMedia(): Observable<Array<IMedia>>{
   httpGetMedia():  Array<IMedia>{
-    const requestUrl = 'http://10.0.211.2:3000/videos';
+    const requestUrl = 'http://11.0.73.2:3000/videos';
     //debugger;   
     //return this.http.get<Array<any>>(requestUrl);
    // return this.http.get<Array<IMedia>>(requestUrl);
