4a40ce58034aae9824545bcd734e12ac98e9a294
Shalil Lior <TC02273@esl.corp.elbit.co.il>
2018-10-08 17:00:10 +0300

    Download video feature

--- src/app/components/video-properties/video-properties-min/video-properties-min.component.ts
+++ src/app/components/video-properties/video-properties-min/video-properties-min.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { IMedia, Updates } from '../../../models/imadia.model';
 import { UsersService } from '../../../services/users.service';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-video-properties-min',
@@ -19,9 +20,9 @@ import { Router } from '@angular/router';
 export class VideoPropertiesMinComponent implements OnInit {
   private userEmail : String;
   @Input() currentItem : IMedia;
-  
+  downloadMessage = '';
 
-  constructor(private router:Router, private userService : UsersService) {}
+  constructor(private router:Router, private userService : UsersService, private http: HttpClient) {}
 
   ngOnInit() {
     this.userEmail = this.userService.getUserEmail();
@@ -40,4 +41,33 @@ export class VideoPropertiesMinComponent implements OnInit {
     event.preventDefault();
     this.router.navigate(['/watch', this.currentItem._id]);
   }
+  saveData(data, fileName) {
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    //a.style = "display: none";
+    const blob = new Blob([data], {type: "video\/mp4"}),
+    url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    // close downloading message
+    this.downloadMessage = '';
+    window.URL.revokeObjectURL(url);
+  }
+  downloadVideo(event) {
+    event.preventDefault();
+    // show downloading message
+    this.downloadMessage = 'Downloading video...';
+    this.http.get('http://11.0.73.2:3000/videos/' + this.currentItem._id, {responseType: 'arraybuffer'})
+      .subscribe((res) => this.saveData(res, this.currentItem.title + '.mp4') );
+  }
+  isShowDownloadMessage() {
+    let myStyles: Object = {};
+    if (this.downloadMessage) {
+      myStyles['display'] = 'block';
+    } else {
+      myStyles['display'] = 'none';
+    }
+    return myStyles;
+  }  
 }
